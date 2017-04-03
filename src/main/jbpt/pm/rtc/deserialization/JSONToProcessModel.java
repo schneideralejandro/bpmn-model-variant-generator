d71cbbfcb7ad95fb969ca7ae92948cbda843430b
Schneider Alejandro <schneideralejandro@outlook.com>
2017-04-03 11:45:02 -0300

    Changed Paths#get(String) to File#toPath() in JSONToProcessModel#deserialize(String)

--- src/main/jbpt/pm/rtc/deserialization/JSONToProcessModel.java
+++ src/main/jbpt/pm/rtc/deserialization/JSONToProcessModel.java
@@ -4,24 +4,18 @@ import org.jbpt.pm.ProcessModel;
 import org.jbpt.pm.io.JSON2Process;
 import org.jbpt.throwable.SerializationException;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class JSONToProcessModel {
-  private String readFile(Path path) throws IOException {
-    byte[] encoded = Files.readAllBytes(path);
-    String contents = new String(encoded, StandardCharsets.UTF_8);
-    return contents;
-  }
-
-  public ProcessModel deserialize(String file)
+  public ProcessModel deserialize(String fileName)
     throws InvalidPathException, IOException, SerializationException {
-    Path path = Paths.get(file);
-    String contents = readFile(path);
+    File file = new File(fileName);
+    byte[] encoded = Files.readAllBytes(file.toPath());
+    String contents = new String(encoded, StandardCharsets.UTF_8);
     ProcessModel model = JSON2Process.convert(contents);
     return model;
   }
